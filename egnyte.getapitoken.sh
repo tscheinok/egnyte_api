a952d1c2e16898bd418caefaa4aecd2d04855b63
Tamir Scheinok <tscheinok@schm03.local>
2019-01-22 17:01:13 -0800

    Added notes about curl requirements on OSX Mojave.

--- egnyte.getapitoken.sh
+++ egnyte.getapitoken.sh
@@ -8,5 +8,6 @@
 #	<yourpassword>	Your egnyte password
 #	<egnytedomain> 	Your Egnyte domain
 #	<yourclientID>. Get this here https://developers.egnyte.com/member/register .  It take a few hours for Egnyte to approve for internal use.
+# 	Note --data-urlencode was required on OSX (mojave).  It is not consistent with Egnyte API docs.
 
 curl -v --data-urlencode 'grant_type=password' --data-urlencode 'username=<yourusername>' --data-urlencode 'password=<yourpassword>' --data-urlencode 'client_id=<yourclientID>' https://<egnytedomain>.egnyte.com/puboauth/token
